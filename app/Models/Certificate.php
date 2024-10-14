760cdfb7f0ece83c203530f280210178bc2c6734
cga-bgp <cga.barokahperkasagroup@gmail.com>
2024-10-14 10:09:23 +0800

    fix model validation for certificate and resign

--- app/Models/Certificate.php
+++ app/Models/Certificate.php
@@ -4,8 +4,29 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
+use Spatie\Permission\Traits\HasRoles;
 
 class Certificate extends Model
 {
-    use HasFactory;
+    use HasFactory, HasRoles;
+
+    protected $fillable = [
+        'crew_id',
+        'certificate_type',
+        'certificate_number',
+        'issue_date',
+        'expiry_date',
+        'certificate_status',
+    ];
+
+    public function crew()
+    {
+        return $this->belongsTo(Crew::class);
+    }
+
+    public function getCertificateDocumentUrlAttribute()
+    {
+        return Storage::url($this->certificate_document);
+    }
 }
--- app/Models/Resign.php
+++ app/Models/Resign.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Permission\Traits\HasRoles;
 
 class Resign extends Model
 {
-    use HasFactory;
+    use HasFactory, HasRoles;
+
+    protected $fillable = [
+        'crew_id',
+        'resign_date',
+        'remark',
+    ];
+
+    public function crew()
+    {
+        return $this->belongsTo(Crew::class);
+    }
 }
