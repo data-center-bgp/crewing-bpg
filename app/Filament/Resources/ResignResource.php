ad75cb7e3c6aa1baeef47c74c75090c2b491c9b9
cga-bgp <cga.barokahperkasagroup@gmail.com>
2024-10-14 09:48:37 +0800

    add resign document feature

---
+++ app/Filament/Resources/ResignResource.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ResignResource\Pages;
+use App\Models\Crew;
+use App\Models\Resign;
+use Filament\Forms;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Support\Facades\Auth;
+
+class ResignResource extends Resource
+{
+    protected static ?string $model = Resign::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Hidden::make('crew_id')
+                    ->default(Auth::user()->crew->id)
+                    ->visible(fn () =>
+                    /** @var \App\Models\User */
+                    Auth::user()->hasRole('crew'))
+                    ->required(),
+                Select::make('crew_id')
+                    ->label('Crew')
+                    ->options(Crew::all()->pluck('name', 'id'))
+                    ->searchable()
+                    ->visible(fn () =>
+                    /** @var \App\Models\User */
+                    Auth::user()->hasRole('super_admin') || Auth::user()->hasRole('admin'))
+                    ->required(),
+                Forms\Components\DatePicker::make('resign_date')
+                    ->required(),
+                Forms\Components\TextInput::make('remark')
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('crew_id')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('resign_date')
+                    ->date()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('remark')
+                    ->searchable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListResigns::route('/'),
+            'create' => Pages\CreateResign::route('/create'),
+            'edit' => Pages\EditResign::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Filament/Resources/ResignResource/Pages/ListResigns.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Resources\ResignResource\Pages;
+
+use App\Filament\Resources\ResignResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ListRecords;
+
+class ListResigns extends ListRecords
+{
+    protected static string $resource = ResignResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make(),
+        ];
+    }
+}
